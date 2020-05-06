142e095a3bb67c994ad5117e2b1b5c024f53a9f7
Israel-Ferreira <israel.souza5@fatec.sp.gov.br>
2020-05-06 00:13:28 -0300

    Desafio Quase Pronto

---
+++ app/controllers/api/v1/delations_controller.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+class Api::V1::DelationsController < ApplicationController
+  before_action :set_delation, only: %i[show update]
+
+  def index
+    @delations = Delation.all 
+    render json: @delations
+  end
+
+  def create
+    @delation = Delation.new(delation_params)
+    @delation.user = current_user
+
+    if @delation.save
+      render json: @delation, status: :created
+    else
+      render json: @delation.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @delation.update(delation_params)
+      render json: @delation
+    else
+      render json: @delation.errors
+    end
+  end
+
+  private
+
+  def delation_params
+    params.require(:delation).permit(:description, :status, :latitude, :longitude)
+  end
+
+  def set_delation
+    @delation = Delation.find(params[:id])
+  end
+end
---
+++ test/models/delation_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class DelationTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
